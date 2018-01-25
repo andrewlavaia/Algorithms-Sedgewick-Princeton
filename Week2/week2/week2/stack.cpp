ea259fdbc5ce27898e22fe08efa2487865f64943
Andrew L <alavaia@yahoo.com>
2018-01-25 08:29:29 -0500

    added LinkedList Stack

--- Week2/week2/week2/stack.cpp
+++ Week2/week2/week2/stack.cpp
@@ -1,14 +1,12 @@
 
 #include <iostream>
 #include <string>
+#include <memory>
 
 class ArrayStack {
 public:
 	bool isEmpty() {
-		if (m_index == 0)
-			return true;
-		else
-			return false;
+		return (m_index == 0);
 	}
 
 	void push(std::string s) {
@@ -17,15 +15,55 @@ public:
 	}
 
 	std::string pop() {
-		std::string s = m_stack[m_index - 1];
-		m_stack[m_index - 1] = "";
-		m_index--;
-		return s;
+		if (!isEmpty()) {
+			std::string s = m_stack[m_index - 1];
+			m_stack[m_index - 1] = "";
+			m_index--;
+			return s;
+		}
+		else {
+			return "can't pop - empty";
+		}
+
 	}
 
 private:
 	int m_index = 0;
 	std::string m_stack[10];
+};
+
+class Node {
+public:
+	std::string s;
+	std::shared_ptr<Node> next;
+};
+
+class LinkedStack {
+public:
+	bool isEmpty() {
+		return (m_first == nullptr);
+	}
+
+	void push(std::string s) {
+		std::shared_ptr<Node> temp = m_first;
+		m_first = std::shared_ptr<Node>(new Node);
+		m_first->s = s;
+		m_first->next = temp;
+	}
+
+	std::string pop() {
+		if (!isEmpty()) {
+			std::string temp = m_first->s;
+			m_first = m_first->next;
+			return temp;
+		}
+		else {
+			return "can't pop - empty";
+		}
+	}
+
+private:
+	std::shared_ptr<Node> m_first;
 };
 
 int main() {
@@ -37,6 +75,20 @@ int main() {
 	arrStack.push("go3");
 	std::cout << arrStack.pop() << std::endl;
 	std::cout << arrStack.pop() << std::endl;
+	std::cout << arrStack.isEmpty() << std::endl;
+	std::cout << arrStack.pop() << std::endl;
+	
+	LinkedStack linkStack;
+	
+	linkStack.push("go1");
+	std::cout << linkStack.pop() << std::endl;
+	linkStack.push("go2");
+	linkStack.push("go3");
+	std::cout << linkStack.isEmpty() << std::endl;
+	std::cout << linkStack.pop() << std::endl;
+	std::cout << linkStack.pop() << std::endl;
+	std::cout << linkStack.isEmpty() << std::endl;
+	std::cout << linkStack.pop() << std::endl;
 	
 	int stackSize = 0;
 	std::cout << "enter 0 to exit" << std::endl;
