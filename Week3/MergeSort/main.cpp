c81f4ac584bd9d5154fb123d5f8b9eb29384db2e
Andrew L <alavaia@yahoo.com>
2018-04-08 22:33:43 -0400

    Added a timer to merge sort

--- Week3/MergeSort/main.cpp
+++ Week3/MergeSort/main.cpp
@@ -1,17 +1,45 @@
 #include <iostream>
+#include <chrono>    // timer functions
+#include <cstdlib>   // rand
 
 #include "mergesort.h"
 
 int main() {
 
+	/*
 	MergeSort ms(10, 100);
 	ms.print();
 	ms.sort();
 	ms.print();
+	*/
 
-	int i = 1;
-	while (i != 0) {
-		std::cin >> i;
+	// Create a size n array and sort it
+
+	int n = 1;
+	int range = 0;
+	int trials = 0;
+
+	while (n != 0) {
+		std::cout << "size of heap: ";
+		std::cin >> n;
+		std::cout << std::endl << "range of heap: ";
+		std::cin >> range;
+		std::cout << std::endl << "trials: ";
+		std::cin >> trials;
+		std::cout << std::endl;
+
+		// Start Timer
+		auto begin = std::chrono::high_resolution_clock::now();
+		for (auto i = 0; i < trials; i++)
+		{
+			// Start Test
+				MergeSort ms(n, range);
+				ms.sort();
+				ms.print();
+		}
+		auto end = std::chrono::high_resolution_clock::now();
+		auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count();
+		std::cout << "duration:    " << duration << "ns." << std::endl;
 	}
 	return 0;
 }
